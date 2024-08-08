becd13a3a7da0869d0a047c34a838feb0a9bf3eb
Anton <115165322+X0sSe20@users.noreply.github.com>
2024-08-08 15:25:06 +0300

    main yaMap
    Добавил карты(почему то 2 штуки по центру).

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Avatar, Button, Card, Flex, Typography } from 'antd';
 import { useAuth } from '../contexts/AuthContext.jsx';
 import { UserOutlined } from "@ant-design/icons";
+import YandexMap from '../yandexMap/YandexMap.jsx'; // Импортируем компонент карты
 
 const Dashboard = () => {
   const { userData, logout} = useAuth();
@@ -11,32 +12,36 @@ const Dashboard = () => {
     
   }
   return(
-    <Card className="profile-card">
-      <Flex vertical gap="small" align="center">
-        <Avatar size={80} icon={<UserOutlined />} className="avatar" />
-        <Typography.Title 
-          level={2}   
-          strong 
-          className="username"
-        >
-          {userData.name}
-        </Typography.Title>
-        <Typography.Text type="secondary" strong>
-          Email: {userData.email}
-        </Typography.Text>
-        <Typography.Text type="secondary">
-          Role: {userData.role}
-        </Typography.Text>
-        <Button 
-          size="large" 
-          type="primary" 
-          className="profile-btn"
-          onClick={handleLogout}
-        >
-          Logout
-        </Button>
-      </Flex>
-    </Card>
+    <div>
+      <Card className="profile-card">
+        <Flex vertical gap="small" align="center">
+          <Avatar size={80} icon={<UserOutlined />} className="avatar" />
+          <Typography.Title 
+            level={2}   
+            strong 
+            className="username"
+          >
+            {userData.name}
+          </Typography.Title>
+          <Typography.Text type="secondary" strong>
+            Email: {userData.email}
+          </Typography.Text>
+          <Typography.Text type="secondary">
+            Role: {userData.role}
+          </Typography.Text>
+          <Button 
+            size="large" 
+            type="primary" 
+            className="profile-btn"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </Flex>
+      </Card>
+
+      <YandexMap /> {/* Встраиваем компонент карты */}
+    </div>
   ); 
 };
 
