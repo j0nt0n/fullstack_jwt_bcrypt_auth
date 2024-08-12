1759d8e0457189d30d09794e9fd6603372a0ebb9
Anton <115165322+X0sSe20@users.noreply.github.com>
2024-08-12 13:10:16 +0300

    add filter product

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,83 +1,99 @@
-import React from 'react';
-import { Avatar, Button, Card, Flex, Typography } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Avatar, Button, Card, Checkbox, Flex, Typography } from 'antd';
 import { useAuth } from '../contexts/AuthContext.jsx';
 import { UserOutlined } from "@ant-design/icons";
-import { YMaps, Map, ObjectManager  } from "@pbe/react-yandex-maps";
+import { YMaps, Map, ObjectManager } from "@pbe/react-yandex-maps";
 
-const mapState = { center: [55.76, 37.64], zoom: 10, controls: ["zoomControl", "fullscreenControl"],};
+const mapState = { center: [55.76, 37.64], zoom: 10, controls: ["zoomControl", "fullscreenControl"] };
 
-const objectManagerFeatures = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      id: 1,
-      geometry: {
-        type: "Point",
-        coordinates: [55.869339, 37.498519]
-      },
-      properties: {
-        balloonContent: `
-          <div>
-            <h3>Точка 1: Общага</h3>
-            <p>Это общежитие, где живут студенты.</p>
-            <img class = "image-map" src="https://avatars.mds.yandex.net/get-altay/2405086/2a000001748c8d0609b0af0b2d7ad1347c2e/L" alt="Общага" />
-          </div>
-        `,
-        hintContent: "Общага",
-      },
-      options: {
-        preset: "islands#greenDotIcon",  // Зеленая метка
-      }
-    },
-    {
-      type: "Feature",
-      id: 2,
-      geometry: {
-        type: "Point",
-        coordinates: [55.856124, 37.555723]
-      },
-      properties: {
-        balloonContent: `
-          <div>
-            <h3>Точка 2: Дом любви</h3>
-            <p>Описание этого места.</p>
-            <img class = "image-map" src="https://avatars.mds.yandex.net/get-altay/2405086/2a000001748c8d0609b0af0b2d7ad1347c2e/L" alt="Дом любви" />
-          </div>
-        `,
-        hintContent: "Дом любви",
-      },
-      options: {
-        preset: 'islands#redDotIcon' // Красная метка
-      }
-    },
-  ]
-};
+const restaurantData = [
+  { 
+    id: 1, 
+    coordinates: [55.869339, 37.498519], 
+    name: "Ресторан 1", 
+    products: ["орехи", "рыба"], 
+    description: "Уютный ресторан с отличной кухней и обслуживанием.",
+    imageUrl: "https://avatars.mds.yandex.net/get-altay/10238647/2a000001904c30be9b6d405d1e7c1e90048b/L"
+  },
+  { 
+    id: 2, 
+    coordinates: [55.856124, 37.555723], 
+    name: "Ресторан 2", 
+    products: ["молоко", "яйца"],
+    description: "Уютный ресторан с отличной кухней и обслуживанием.",
+    imageUrl: "https://avatars.mds.yandex.net/get-altay/10238647/2a000001904c30be9b6d405d1e7c1e90048b/L"
+  },
+  { 
+    id: 3, 
+    coordinates: [55.751244, 37.618423], 
+    name: "Ресторан 3", 
+    products: ["орехи", "яйца"],
+    description: "Уютный ресторан с отличной кухней и обслуживанием.",
+    imageUrl: "https://avatars.mds.yandex.net/get-altay/10238647/2a000001904c30be9b6d405d1e7c1e90048b/L"
+  },
+];
 
 const Dashboard = () => {
-  const { userData, logout} = useAuth();
+  const { userData, logout } = useAuth();
+  const [allergens, setAllergens] = useState([]);
+  const [updateKey, setUpdateKey] = useState(0); // Состояние для обновления карты
 
   const handleLogout = async () => {
     await logout();
-    
-  }
-  return(
+  };
+
+  const handleAllergenChange = (e) => {
+    const value = e.target.value;
+    setAllergens(prevAllergens =>
+      prevAllergens.includes(value)
+        ? prevAllergens.filter(item => item !== value)
+        : [...prevAllergens, value]
+    );
+  };
+
+  const objectManagerFeatures = {
+    type: "FeatureCollection",
+    features: restaurantData.map(restaurant => {
+      const hasAllergen = restaurant.products.some(product => allergens.includes(product));
+      return {
+        type: "Feature",
+        id: restaurant.id,
+        geometry: { type: "Point", coordinates: restaurant.coordinates },
+        properties: {
+          balloonContent: `
+            <div>
+              <h3>${restaurant.name}</h3>
+              ${restaurant.description ? `<p>${restaurant.description}</p>` : ''}
+              ${restaurant.imageUrl ? `<img src="${restaurant.imageUrl}" alt="${restaurant.name}" style="width:100px;height:auto;" />` : ''}
+              <p>Продукты: ${restaurant.products.join(", ")}</p>
+            </div>
+          `,
+          hintContent: restaurant.name,
+        },
+        options: {
+          preset: hasAllergen ? 'islands#redDotIcon' : 'islands#greenDotIcon',
+        },
+      };
+    }),
+  };
+
+  const handleRefreshMap = () => {
+    setUpdateKey(prevKey => prevKey + 1); // Увеличиваем ключ для перерисовки карты
+  };
+
+  return (
     <div>
       <Card className="profile-card">
         <Flex vertical gap="small" align="center">
           <Avatar size={80} icon={<UserOutlined />} className="avatar" />
-          <Typography.Title 
-            level={2}   
-            strong 
-            className="username"
-          >
+          <Typography.Title level={2} strong className="username">
             {userData.name}
           </Typography.Title>
           <Typography.Text type="secondary" strong>
-            Email: {userData.email}
+            Почта: {userData.email}
           </Typography.Text>
           <Typography.Text type="secondary">
-            Role: {userData.role}
+            Роль: {userData.role}
           </Typography.Text>
           <Button 
             size="large" 
@@ -85,33 +101,40 @@ const Dashboard = () => {
             className="profile-btn"
             onClick={handleLogout}
           >
-            Logout
+            Выход
           </Button>
+
+          <Card className="allergen-card">
+            <Typography.Title level={4} strong>Выбор аллергенов или продуктов которые сегодня бы не хотели:</Typography.Title>
+            <div>
+              <Checkbox value="орехи" onChange={handleAllergenChange}>орехи</Checkbox>
+              <Checkbox value="яйца" onChange={handleAllergenChange}>яйца</Checkbox>
+              <Checkbox value="молоко" onChange={handleAllergenChange}>молоко</Checkbox>
+            </div>
+            <Button 
+              type="primary" 
+              onClick={handleRefreshMap} 
+              style={{ marginTop: '16px' }}
+            >
+              Обновить карту
+            </Button>
+          </Card>
         </Flex>
       </Card>
 
       <Card className="yandex-map">
-      <YMaps>
-        <Map width="950px" height="750px" state={mapState} modules={["control.ZoomControl", "control.FullscreenControl"]}>
-        <ObjectManager 
-          options={{ 
-          clusterize: true, 
-          gridSize: 32, 
-        }} 
-        objects={{ 
-          openBalloonOnClick: true, 
-        }} 
-        clusters={{ 
-          preset: "islands#redClusterIcons", 
-        }} 
-        defaultFeatures={objectManagerFeatures} 
-        modules={[ 
-          "objectManager.addon.objectsBalloon", 
-          "objectManager.addon.objectsHint", 
-        ]} 
-        />
-        </Map>
-      </YMaps>
+        <YMaps>
+          <Map width="950px" height="750px" state={mapState} modules={["control.ZoomControl", "control.FullscreenControl"]}>
+            <ObjectManager 
+              key={updateKey} // Используем ключ для принудительного обновления
+              options={{ clusterize: true, gridSize: 32 }} 
+              objects={{ openBalloonOnClick: true }} 
+              clusters={{ preset: "islands#redClusterIcons" }} 
+              features={objectManagerFeatures} // Используем features вместо defaultFeatures
+              modules={[ "objectManager.addon.objectsBalloon", "objectManager.addon.objectsHint" ]}
+            />
+          </Map>
+        </YMaps>
       </Card>
     </div>
   ); 
